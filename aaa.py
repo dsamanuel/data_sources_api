f3ee983d8ef661ca1fcc8021538f17d2bc0aebae
dsamanuel <habtamuwh@gmail.com>
2026-05-17 19:17:42 +0300

    commcare api update2

--- aaa.py
+++
@@ -1,9 +0,0 @@
-def find_max(numbers):
-    max_val = numbers[0]  # Potential issue here
-    for n in numbers:
-        if n > max_val:
-            max_val = n
-    return max_val
-
-aa = find_max([-5, -10, -2])  # This will return -2, but if the list had positive numbers, it would fail
-print(aa)
